83a94b0aa1fc2db4487b3736a4ae4742cc17103c
stefanodt <sditoro@roc-dev.com>
2018-08-29 13:10:28 +0100

    'hoi'

---
+++ src/h01/HalloRocDev.java
@@ -0,0 +1,11 @@
+package h01;
+
+/**
+ * Created by  on 31-08-16.
+ */
+public class HalloRocDev {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo stefano ditoro!");
+    }
+}
